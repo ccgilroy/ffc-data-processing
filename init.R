ebfb3e70b9b211060ae81ea79862d6a61f239cad
Connor Gilroy <ccgilroy@gmail.com>
2017-07-10 15:36:14 -0700

    comment and tweet init.R

--- init.R
+++ init.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(forcats)
 library(haven)
 library(labelled)
 library(readr)
@@ -6,6 +7,7 @@ library(stringr)
 
 source("R/convert_classes.R")
 source("R/get_vars.R")
+source("R/merge_train.R")
 source("R/process_data.R")
 source("R/recode_na.R")
 source("R/subset_vars.R")
@@ -13,11 +15,22 @@ source("R/summarize_variables.R")
 
 background <- read_dta("data/background.dta")
 
+# example 1 ----
+# process all variables, leaving ambiguous variables as characters
+# does not handle missing values, does not remove any variables
+# then summarize variable type information and write to csv
+
 background_processed <- process_data_minimal(background)
 variable_types <- summarize_variables(background, background_processed)
 
 if (!dir.exists("output")) dir.create("output")
 write_csv(variable_types, "output/ffc_variable_types.csv")
+
+# example 2 ----
+# subset to constructed variables
+# process by converting all variables, removing all NAs,
+# and removing variables that are unusable for modeling
+# then merge with train
 
 train <- read_csv("data/train.csv")
 ffc <- 
